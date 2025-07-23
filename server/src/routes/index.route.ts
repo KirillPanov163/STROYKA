972d4bccca0f78fe539d50dbfea2a242999ead7b
Dencheny123 <avgustandzoker@gmail.com>
2025-07-24 00:46:24 +0300

    Обновлены зависимости ESLint в package.json и package-lock.json. Изменены модели в Prisma: переименованы MetaData, FAQ, SERVICE, MY_WORK и CONTACTS в Meta_data, Faq, Service, My_work и Contacts соответственно. Добавлены новые маршруты для работы с метаданными и услугами.

--- server/src/routes/index.route.ts
+++ server/src/routes/index.route.ts
@@ -2,11 +2,15 @@ import { Router, Request, Response } from 'express';
 import formatResponse from '../utils/formatResponse.js';
 import authRouter from './auth.route.js';
 import recordingRouter from './recording.route.js';
+import metaDataRouter from './metaData.route.js';
+import serviceRouter from './service.route.js';
 
 const router = Router();
 
 router.use('/auth', authRouter);
-router.use('/recording', recordingRouter)
+router.use('/recording', recordingRouter);
+router.use('/metadata', metaDataRouter);
+router.use('/service', serviceRouter);
 
 router.use((req: Request, res: Response) => {
   res.status(404).json(formatResponse(404, 'Not found'));
