141dc76dd0aacd276bcf68825e41d6fb39047686
7Eighty <igrovoi.ac@yandex.ru>
2025-07-24 18:30:53 +0300

    FAQ ThunkApi

--- client/src/entities/FAQ/api/faqThunkApi.ts
+++ client/src/entities/FAQ/api/faqThunkApi.ts
@@ -1,1 +1,15 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+import { Faq } from '../model/faqTypes';
 
+export const fetchFaqs = createAsyncThunk<Faq[]>(
+  'faq/fetchFaqs',
+  async (_, { rejectWithValue }) => {
+    try {
+      const response = await axios.get<Faq[]>('/api/faq');
+      return response.data;
+    } catch (error: any) {
+      return rejectWithValue(error.message || 'Ошибка запроса на FAQ');
+    }
+  },
+);
