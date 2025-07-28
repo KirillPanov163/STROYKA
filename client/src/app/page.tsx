03d32811572a38c65cb37e9ea1a3f138fcd81dc5
nick123554 <kolya21@mail.ru>
2025-07-28 22:17:16 +0300

    slider

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -2,12 +2,14 @@ import Image from 'next/image';
 import styles from './page.module.css';
 import { ContactsPageComponents } from '@/pages/recordingPage/ContactsPage';
 import FAQPage from './faq/page';
+import Slider from '@/widgets/Slider/Slider';
 
 
 export default function Home() {
   return (
     <div className={styles.page}>
         <ContactsPageComponents/>
+        <Slider/>
         <FAQPage/>
     </div>
   );
