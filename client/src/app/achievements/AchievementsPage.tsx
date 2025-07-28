0d636d713bb89f1441a8d823749914bb53fde9b5
7Eighty <igrovoi.ac@yandex.ru>
2025-07-28 17:30:38 +0300

    add achievementPage and styles in mainPage

---
+++ client/src/app/achievements/AchievementsPage.tsx
@@ -0,0 +1,16 @@
+'use client';
+
+import React from 'react';
+
+import AchievementsSection from '@/entities/achievements/AchievementsSection';
+
+
+const AchievementsPage: React.FC = () => {
+  return (
+    <div className="achievements-page">
+      <AchievementsSection />
+    </div>
+  );
+};
+
+export default AchievementsPage;
